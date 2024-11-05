93216ee13239f9eb8aa6c6f2b2e232e1d7f858c8
wind <jamwujustyle@gmail.com>
2024-11-05 18:42:03 +0500

    __main__

--- index.py
+++ index.py
@@ -9,6 +9,11 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
 )
 from PyQt5.QtCore import Qt
+from smart_contract import (
+    add_average,
+    get_averages_count,
+    get_all_averages,
+)
 
 
 class WeatherApp(QWidget):
@@ -17,9 +22,12 @@ class WeatherApp(QWidget):
         self.city_label = QLabel("Enter city name: ", self)
         self.city_input = QLineEdit(self)
         self.get_weather_button = QPushButton("Get Weather", self)
+        self.store_average_button = QPushButton("Store Average", self)
         self.temperature_label = QLabel(self)
         self.emoji_label = QLabel(self)
         self.description_label = QLabel(self)
+        self.average_label = QLabel(self)
+        self.average = None
         self.initUI()
 
     def initUI(self):
@@ -30,13 +38,16 @@ class WeatherApp(QWidget):
         vbox.addWidget(self.city_label)
         vbox.addWidget(self.city_input)
         vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.store_average_button)
         vbox.addWidget(self.temperature_label)
         vbox.addWidget(self.emoji_label)
         vbox.addWidget(self.description_label)
+        vbox.addWidget(self.average_label)
 
         self.setLayout(vbox)
 
         self.city_label.setAlignment(Qt.AlignCenter)
+        self.average_label.setAlignment(Qt.AlignCenter)
         self.city_input.setAlignment(Qt.AlignCenter)
         self.temperature_label.setAlignment(Qt.AlignCenter)
         self.emoji_label.setAlignment(Qt.AlignCenter)
@@ -45,6 +56,8 @@ class WeatherApp(QWidget):
         self.city_label.setObjectName("city_label")
         self.city_input.setObjectName("city_input")
         self.get_weather_button.setObjectName("get_weather_button")
+        self.store_average_button.setObjectName("store_average_button")
+        self.average_label.setObjectName("average_label")
         self.temperature_label.setObjectName("temperature_label")
         self.emoji_label.setObjectName("emoji_label")
         self.description_label.setObjectName("description_label")
@@ -54,14 +67,14 @@ class WeatherApp(QWidget):
                     QLabel, QPushButton{
                            font-family: calibri;
                     }
-                    QLabel#city_label{
+                    QLabel#city_label, #average_label{
                             font-size: 40px;
                             font-style: italic;
                     }
                     QLineEdit#city_input{
                     font-size: 40px;
                     }
-                    QPushButton#get_weather_button{
+                    QPushButton#get_weather_button, #store_average_button{
                     font-size: 30px;
                     font-weight: bold;
                     }
@@ -79,6 +92,16 @@ class WeatherApp(QWidget):
         )
 
         self.get_weather_button.clicked.connect(self.get_weather)
+        self.store_average_button.clicked.connect(self.store_average)
+
+    def store_average(self):
+        if self.average is not None:
+            add_average(int(self.average))
+            self.average_label.setText(
+                f"Average {self.average} stored to smart contract"
+            )
+        else:
+            self.average_label.setText("Average temperature not calculated yet")
 
     def get_weather(self):
 
@@ -154,8 +177,8 @@ class WeatherApp(QWidget):
         temperature_c = round(temperature_k - 273.15, 1)
         temp2 = data2["current"]["temp_c"]
         print(f"from second api {temp2}")
-        average = (temperature_c + temp2) / 2
-        print(f"average is {average:.1f}")
+        self.average = (temperature_c + temp2) / 2
+        self.average_label.setText(f"Calculated average: {self.average}")
 
     @staticmethod
     def get_weather_emoji(weather_id):
--- smart_contract.py
+++ smart_contract.py
@@ -1,1 +1,82 @@
 from web3 import Web3
+
+infura_url = "https://sepolia.infura.io/v3/9e46cd5c5c944f31a9738202e83c0e0b"
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+
+if web3.is_connected():
+    print("Successfully connected to Infura Sepolia")
+else:
+    print("failed to connect to Infura Sepolia")
+
+abi = [
+    {
+        "inputs": [{"internalType": "int256", "name": "average", "type": "int256"}],
+        "name": "addAverage",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "name": "averages",
+        "outputs": [{"internalType": "int256", "name": "", "type": "int256"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+    {
+        "inputs": [],
+        "name": "getAllAverages",
+        "outputs": [{"internalType": "int256[]", "name": "", "type": "int256[]"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+    {
+        "inputs": [{"internalType": "uint256", "name": "index", "type": "uint256"}],
+        "name": "getAverage",
+        "outputs": [{"internalType": "int256", "name": "", "type": "int256"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+    {
+        "inputs": [],
+        "name": "getAveragesCount",
+        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function",
+    },
+]
+
+contract_address = "0xf4Bb3bFB524E76e66BC2860944100431DF3B4880"
+
+contract = web3.eth.contract(address=contract_address, abi=abi)
+wallet_address = "0x74fBBb0Be04653f29bd4b2601431E87f9B811319"
+
+
+def add_average(value):
+    txn = contract.functions.addAverage(value).build_transaction(
+        {
+            "chainId": 11155111,
+            "gas": 2000000,
+            "gasPrice": web3.to_wei("20", "gwei"),
+            "nonce": web3.eth.get_transaction_count(wallet_address),
+        }
+    )
+    signed_txn = web3.eth.account.sign_transaction(
+        txn,
+        private_key="00be00eb4bbd4c9f28e744f01efbeec08914f252ae7669c50f441905331a835b",
+    )
+    tx_hash = web3.eth.send_raw_transaction(signed_txn.raw_transaction)
+    print("Transaction hash:", tx_hash.hex())
+
+
+def get_averages_count():
+    return contract.functions.getAveragesCount().call()
+
+
+def get_average(index):
+    return contract.functions.getAverage(index).call()
+
+
+def get_all_averages():
+    return contract.functions.getAllAverages().call()
